0cc55525e44f41a4f941f802b434ee282c908631
BegaliyevXushnud <xushnudpubgmn1@gmail.com>
2024-06-23 05:44:41 +0500

    added about

--- Js/app.js
+++ Js/app.js
@@ -1,6 +1,8 @@
 const barses = document.querySelector('.barses')
 const headerList = document.querySelector('.header-list')
+const over = document.getElementById('overplay')
 
 barses.addEventListener('click', () => {
     headerList.classList.toggle('header_toogle')
+    over.classList.toggle('overplay')
 })
